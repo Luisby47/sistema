db255df8d612bee4894049ea151d122c5c93150b
Luisby47 <75922735+Luisby47@users.noreply.github.com>
2024-09-28 21:17:10 -0600

    FIX VITE

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./resources/js/**/*.js', './resources/views/**/*.blade.php','./resources/**/*.vue'],
+  content: ['./resources/js/**/*.js', './resources/views/**/*.blade.php'],
   darkMode: 'class',
   theme: {
     screens: {
