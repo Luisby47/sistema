1a025d64224546c487c11c7dce6d28c6a9da4259
dall_dev <randrmz.dev13@gmail.com>
2025-03-17 22:43:24 -0600

    Get Employee from cache
    Get Employee from cache in conceptosEmployee in controller

--- app/Http/Controllers/Calculos/ControllerCalculos.php
+++ app/Http/Controllers/Calculos/ControllerCalculos.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers\Calculos;
 
 use App\Http\Controllers\Controller;
+use App\Models\CrnubeSpreadsheetConceptosEmployee;
+use App\Models\CrnubeSpreedsheatConceptos;
+use App\Models\HrEmployee;
+use http\Client\Request;
+use Illuminate\Support\Facades\Cache;
 use PHPUnit\Event\Telemetry\System;
 
 class ControllerCalculos extends Controller
 {
+
     public function generarCalculos()
     {
+        $id = Cache::get('selected_employee');
+        $employee = HrEmployee::find($id);
 
-
-        echo "Hola, mundo!";
-
-
+        echo "Hola, $employee";
        // return back();
-
     }
 }
